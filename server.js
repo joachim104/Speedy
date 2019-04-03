11c534da2374b647a00e9d7cacfe5e5a0c777f55
joachim104 <joachim.stougaard@hotmail.com>
2019-04-03 18:30:16 +0200

    app commited

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const speedTest = require('speedtest-net');
+const app = express()
+const port = 3000
+const public = app.use(express.static(__dirname + '/public'));
+
+let internetSpeedData;
+
+TestInternetSpeed();
+
+// Runs test on the users internet speed and saves to variable
+function TestInternetSpeed() {
+    speedTest.visual({ maxTime: 6000 }, (err, data) => {
+        internetSpeedData = data;
+    });
+}
+
+// Runs speedtest every 15. second 
+setInterval(function () {
+    // TestInternetSpeed();
+}, 15000);
+
+// Sends internet speed variable to client
+app.get('/internet-speed', (req, res) =>
+    res.send(internetSpeedData)
+);
+
+
+app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
+
+app.listen(port, () => console.log(`Server started on port ${port}!`));
